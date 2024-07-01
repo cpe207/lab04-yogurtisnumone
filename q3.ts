5e50782218644bc053192f846800e7a809d18d69
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-07-01 14:24:34 +0700

    กชภัส พันธ์ทอง 660612137

--- q3.ts
+++ q3.ts
@@ -1,6 +1,16 @@
 function primeNumber(a) {
-  /* Your code here */
+  if( a <= 1 ){
+    return "NO";
+  }else{
+    for( let i = 2 ; i < a ; i++ ){
+      if ( a%i === 0 ){
+        return "NO";
+      }
+    }return "YES";
+  }
+  
 }
+
 
 const d1 = 10;
 const d2 = 29;
